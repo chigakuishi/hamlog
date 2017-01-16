30c9d7d6a045e1c8b426c79bde8b0897b44239c0
柴田彩香 <shibataayaka@MacBook-Air-Ayaka-Shibata.local>
2017-01-16 17:04:35 +0900

    変更しました

--- front/js/input.js
+++ front/js/input.js
@@ -1,3 +1,7 @@
+var sentences;
+var datas = {};
+var ele ={};
+
 function openButton(){
 	document.getElementById('buttons').style.display="block";	
 	document.getElementById('save').disabled="disabled";	
@@ -6,12 +10,51 @@ function openButton(){
 function changeData(){
 	document.getElementById('convert').disabled ="disabled";	
 	document.getElementById('save').disabled ="";	
+	sentences = document.getElementById('data').value;
+	dataList = sentences.split(/\n/);
+	for(var i=0;i<dataList.length;i=i+1){
+		ele = dataList[i].split(":");
+		datas[ele[0]] = ele[1];
+	}
 }
 
 function saveData(){
 	document.getElementById('save').disabled ="disabled";	
 	document.getElementById('convert').disabled ="";	
 	window.location.reload();
-  var data = document.getElementById("data").value
 }
 
+/*
+var obj ={
+  "main":{
+    "callsign":str,
+    "date":int,
+    "time":int,
+    "gth":str,
+    "freq":double,
+    "mode":str,
+    "my-gth":str,
+    "rst":int,
+  },
+
+  "option":{
+    "name":str,
+    "ant":obj,
+    "rig":str,
+    "power":double,
+    "qsl":bool,
+    "first":bool,
+    "home":str,
+    "aniv":str,
+    "oneway":str,
+  },
+
+  "other":{
+		"work": ,
+  },
+
+	"other-free":{
+		"","","","",""		
+	}
+}
+*/
