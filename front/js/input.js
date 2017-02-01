8d107dba7ec0f0539d6c5607447bd789ca12327f
Rhenium <chigakuishi.soft@gmail.com>
2017-02-01 16:25:27 +0900

    大文字対応

--- front/js/input.js
+++ front/js/input.js
@@ -84,6 +84,7 @@ function changeData(){
 		if(dataList[i].match(/^-{3,}$/)){	//"---"で区切る
       obj.main.my_qth = obj.main.my_qth ||def.main.my_qth;
       obj.main.qth = obj.main.qth ||def.main.my_qth;
+      obj.main.callsign = obj.main.callsign.toUpperCase();
       sday = obj.main.date;
 		  calls.push(obj);
 			obj ={
